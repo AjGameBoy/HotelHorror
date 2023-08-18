847695d1e9695386404c4173c0f596653506b297
David Barnes <davidbarnes482@gmail.com>
2023-08-18 15:39:32 +0100

    - Add input key for interactor

--- Assets/Mini First Person Controller/Scripts/Interactor.cs
+++ Assets/Mini First Person Controller/Scripts/Interactor.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Mini_First_Person_Controller.Scripts
 {
     public class Interactor: MonoBehaviour
     {
-        
+        [SerializeField] private InputAction actionKey;
     }
 }
